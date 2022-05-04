6d79c2b3b61543fef53ccd6d311ddd06ed29079d
rubensb-git <rubens.bento@gmail.com>
2022-05-04 15:02:45 -0300

    Alteração para a biblioteca Lombok e para o JUnit5

--- src/test/java/com/springreact/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/springreact/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -1,16 +1,19 @@
 package com.springreact.minhasfinancas.model.repository;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.springreact.minhasfinancas.model.entity.Usuario;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class UsuarioRepositoryTest {
@@ -22,9 +25,7 @@ public class UsuarioRepositoryTest {
   public void deveVerificarAExistenciaDeUmEmail() {
 	  
 	  //cenário 
-	  Usuario usuario = new Usuario();
-	  usuario.setNome("usuario");
-	  usuario.setEmail("usuario@email.com");
+	  Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
 	  repository.save(usuario);
 	  
 	  //ação / execução
